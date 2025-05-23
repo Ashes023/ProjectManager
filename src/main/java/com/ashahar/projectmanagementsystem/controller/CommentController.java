82fc289e1094d4c0bf44de7d3677aac18524fb58
Ashes023 <ashaharyunuskhan@gmail.com>
2025-05-23 08:42:05 +0530

    Application file rmvd

--- src/main/java/com/ashahar/projectmanagementsystem/controller/CommentController.java
+++ src/main/java/com/ashahar/projectmanagementsystem/controller/CommentController.java
@@ -1,4 +1,21 @@
 package com.ashahar.projectmanagementsystem.controller;
 
+import com.ashahar.projectmanagementsystem.service.CommentService;
+import com.ashahar.projectmanagementsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private UserService userService;
+
+
+
 }
